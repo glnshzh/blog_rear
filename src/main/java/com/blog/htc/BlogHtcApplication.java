4eb8a8d18f125276e00e869a94e0e231758f45fd
glnshzh <478184350@qq.com>
2024-04-23 19:14:21 +0800

    first

---
+++ src/main/java/com/blog/htc/BlogHtcApplication.java
@@ -0,0 +1,13 @@
+package com.blog.htc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogHtcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogHtcApplication.class, args);
+    }
+
+}
